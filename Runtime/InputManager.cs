675ca83e15f5bc941c7a4491af12048028e1a77a
12982_gamedev (Student) <s_12982_gamedev@PEDAGOGIQUE.LAN>
2024-07-03 12:37:40 +0200

    save

--- Runtime/InputManager.cs
+++ Runtime/InputManager.cs
@@ -12,6 +12,14 @@ public class MoveCubeFromInpuMono : MonoBehaviour
     public InputActionReference m_input;
     public BoolChangeEvent m_isInput;
 
+    //add angle
+    public InputActionReference m_addangle;
+    public BoolChangeEvent m_isaddangle;
+
+    //remove agnle
+    public InputActionReference m_removeangle;
+    public BoolChangeEvent m_isremoveangle;
+
 
     [System.Serializable]
     public class BoolChangeEvent
@@ -50,8 +58,21 @@ public class MoveCubeFromInpuMono : MonoBehaviour
         m_input.action.Enable();
         m_input.action.performed += Thruster;
         m_input.action.canceled += Thruster;
+
+        //Forward
+        m_addangle.action.Enable();
+        m_addangle.action.performed += AddAngle;
+        m_addangle.action.canceled += AddAngle;
+
+        //Forward
+        m_removeangle.action.Enable();
+        m_removeangle.action.performed += RemoveAngle;
+        m_removeangle.action.canceled += RemoveAngle;
+
     }
 
 
     private void Thruster(InputAction.CallbackContext context) => m_isInput.SetNewValue(context.ReadValueAsButton());
+    private void AddAngle(InputAction.CallbackContext context) => m_isaddangle.SetNewValue(context.ReadValueAsButton());
+    private void RemoveAngle(InputAction.CallbackContext context) => m_isremoveangle.SetNewValue(context.ReadValueAsButton());
 }
