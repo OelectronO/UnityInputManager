f46ae23c09f4dd85476c969912fad0821e7ac58a
12982_gamedev (Student) <s_12982_gamedev@PEDAGOGIQUE.LAN>
2024-07-03 11:37:07 +0200

    save

--- Runtime/InputManager.cs
+++ Runtime/InputManager.cs
@@ -9,20 +9,8 @@ using static Codice.Client.Commands.WkTree.WorkspaceTreeNode;
 public class MoveCubeFromInpuMono : MonoBehaviour
 {
     //Thruster
-    public InputActionReference m_thruster;
-    public BoolChangeEvent m_isThruster;
-
-    //Yaw
-    public InputActionReference m_yaw;
-    public BoolChangeEvent m_isYaw;
-
-    //Pitch
-    public InputActionReference m_pitch;
-    public BoolChangeEvent m_isPitch;
-
-    //Roll
-    public InputActionReference m_roll;
-    public BoolChangeEvent m_isRoll;
+    public InputActionReference m_input;
+    public BoolChangeEvent m_isInput;
 
 
     [System.Serializable]
@@ -59,29 +47,11 @@ public class MoveCubeFromInpuMono : MonoBehaviour
     public void OnEnable()
     {
         //Forward
-        m_thruster.action.Enable();
-        m_thruster.action.performed += Thruster;
-        m_thruster.action.canceled += Thruster;
-
-        //Yaw
-        m_yaw.action.Enable();
-        m_yaw.action.performed += Yaw;
-        m_yaw.action.canceled += Yaw;
-
-        //Pitch
-        m_pitch.action.Enable();
-        m_pitch.action.performed += Pitch;
-        m_pitch.action.canceled += Pitch;
-
-        //Roll
-        m_roll.action.Enable();
-        m_roll.action.performed += Roll;
-        m_roll.action.canceled += Roll;
+        m_input.action.Enable();
+        m_input.action.performed += Thruster;
+        m_input.action.canceled += Thruster;
     }
 
 
-    private void Thruster(InputAction.CallbackContext context) => m_isThruster.SetNewValue(context.ReadValueAsButton());
-    private void Yaw(InputAction.CallbackContext context) => m_isYaw.SetNewValue(context.ReadValueAsButton());
-    private void Pitch(InputAction.CallbackContext context) => m_isPitch.SetNewValue(context.ReadValueAsButton());
-    private void Roll(InputAction.CallbackContext context) => m_isRoll.SetNewValue(context.ReadValueAsButton());
+    private void Thruster(InputAction.CallbackContext context) => m_isInput.SetNewValue(context.ReadValueAsButton());
 }
